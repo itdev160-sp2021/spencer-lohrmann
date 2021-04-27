2e30e4b1b9217688aaf98ebf094fbb61ab799af2
Spencer Lohrmann <lohrmans@gmatc.matc.edu>
2021-04-27 12:06:48 -0500

    Completed Activity 8

---
+++ activity-8/js/scripts.js
@@ -0,0 +1,69 @@
+//Utility functions
+function get(element) {
+    return document.getElementById(element);
+}
+
+//Application Functions
+function openModal() {
+    var modal = get('modal-dialog');
+    var backdrop = get ('modal-backdrop');
+
+    modal.classList.add('visible');
+    backdrop.classList.add('visible');
+}
+
+function closeModal() {
+    var title = get('edit-title-text');
+    var text = get('edit-content-text');
+    var modal = get('modal-dialog');
+    var backdrop = get('modal-backdrop');
+
+    //Clear text
+    title.value = '';
+    text.value = '';
+
+    //Hide Modal
+    modal.classList.remove('visible');
+    backdrop.classList.remove('visible');
+}
+
+function saveContent() {
+    var title = get('edit-title-text');
+    var text = get('edit-content-text');
+    var content = get('display-content');
+
+    //Create content elements
+    var newTitle = document.createElement('h2');
+    var newTitleText = document.createTextNode(title.value);
+    var newContent = document.createElement('p');
+    var newContentText = document.createTextNode(text.value);
+
+    newTitle.appendChild(newTitleText);
+    newContent.appendChild(newContentText);
+    content.appendChild(newTitle);
+    content.appendChild(newContent);
+
+    closeModal();
+}
+
+//saveButton is clicked if Enter and Control are pressed at the same time.
+function clickSaveButton(event) {
+    var saveButton = get('save-button');
+
+    if (event.ctrlKey && event.keyCode === 13) {
+        saveButton.click();
+    }
+}
+
+//Wire up event handlers
+window.addEventListener('load', function() {
+    var newButton = get('new-button');
+    var cancelButton = get('cancel-button');
+    var saveButton = get('save-button');
+    var contentText = get('edit-content-text');
+
+    newButton.addEventListener('click', openModal);
+    cancelButton.addEventListener('click', closeModal);
+    saveButton.addEventListener('click', saveContent); 
+    contentText.addEventListener('keydown', clickSaveButton);
+});
